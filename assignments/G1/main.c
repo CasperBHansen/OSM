2682794cdf8d67a89ef70cce2d24584d5d84ad0e
Anders Kiel Hovgaard <akhovg@gmail.com>
2014-02-12 12:47:59 +0100

    minor

--- assignments/G1/main.c
+++ assignments/G1/main.c
@@ -13,31 +13,30 @@
 
 int things[NUM_ITEMS] = { 1, 2, 4, 8 };
 
-bool match_int(item * arg)
-{
-	return (* (int *)arg == 4) ? 1 : 0;
+bool match_int(item * arg) {
+    return (* (int *) arg == 4) ? 1 : 0; // true or false
 }
 
 int main(int argc, char * argv[]) {
 
     dlist *list = malloc(sizeof(dlist));
-	
-	int i = 0;
-	while (i < NUM_ITEMS) insert(list, &things[i++], 0);
-	
-	item * match = NULL;
-	match = search(list, match_int);
-	if (match) printf("Item %d found\n", * (int *)match);
-	
-	reverse(list);
-	match = search(list, match_int);
-	if (match) printf("Item %d found\n", * (int *)match);
-	
-	item * thing = NULL;
-	thing = extract(list, 0);
-	if (thing) printf("Extracted item %i from the head.\n", * (int *)thing);
-	thing = extract(list, 1);
-	if (thing) printf("Extracted item %i from the tail.\n", * (int *)thing);
-	
-	return EXIT_SUCCESS;
+    
+    int i = 0;
+    while (i < NUM_ITEMS) insert(list, &things[i++], 0);
+    
+    item * match = NULL;
+    match = search(list, match_int);
+    if (match) printf("Item %d found\n", * (int *)match);
+    
+    reverse(list);
+    match = search(list, match_int);
+    if (match) printf("Item %d found\n", * (int *)match);
+    
+    item * thing = NULL;
+    thing = extract(list, 0);
+    if (thing) printf("Extracted item %i from the head.\n", * (int *)thing);
+    thing = extract(list, 1);
+    if (thing) printf("Extracted item %i from the tail.\n", * (int *)thing);
+    
+    return EXIT_SUCCESS;
 }
