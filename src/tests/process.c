5719853ae535ce93af1874c14b32a8d3291321e7
Anders Kiel Hovgaard <akhovg@gmail.com>
2014-02-24 20:44:48 +0100

    updated tests, still testing

--- src/tests/process.c
+++ src/tests/process.c
@@ -4,14 +4,21 @@
 
 int main(void)
 {
+    char *exec_str = "\n\nPerforming exec of subprocess..\n";
+    syscall_write(FILEHANDLE_STDOUT, exec_str, sizeof(char) * 35);
+
     int subp_pid = syscall_exec("[arkimedes]subprocess");
 
-    char * str = "XX\n";
-    syscall_write(FILEHANDLE_STDOUT, str, sizeof(const char) * 4);
+    char *exec_str1 = "\n\nReturned from exec syscall..\n";
+    syscall_write(FILEHANDLE_STDOUT, exec_str1, sizeof(char) * 32);
+
+    char *join_str = "\n\nPerforming join with subprocess..\n";
+    syscall_write(FILEHANDLE_STDOUT, join_str, sizeof(char) * 37);
 
     syscall_join(subp_pid);
     
-    syscall_write(FILEHANDLE_STDOUT, str, sizeof(const char) * 4);
+    char *done_str = "\n\nDone with join, returning..\n";
+    syscall_write(FILEHANDLE_STDOUT, done_str, sizeof(char) * 31);
     
     return 0;
 }
--- src/tests/subprocess.c
+++ src/tests/subprocess.c
@@ -8,10 +8,9 @@
 #include "tests/lib.h"
 
 int main() {
-      
+
     const char * hello_str = "Hello sub-process!";
     syscall_write(FILEHANDLE_STDOUT, hello_str, sizeof(const char) * 18);
-  
-    return 1;
+
+    return 42;
 }
-                                        
