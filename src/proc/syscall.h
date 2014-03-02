022d337073e2c3e00ebb39471ccccd6140c5b2e8
Casper B. Hansen <casperbhansen@me.com>
2014-03-02 11:01:24 +0100

    Added handle id's for semaphore syscalls.

--- src/proc/syscall.h
+++ src/proc/syscall.h
@@ -53,6 +53,9 @@
 #define SYSCALL_WRITE 0x205
 #define SYSCALL_CREATE 0x206
 #define SYSCALL_DELETE 0x207
+#define SYSCALL_SEM_OPEN 0x300
+#define SYSCALL_SEM_PROCURE 0x301
+#define SYSCALL_SEM_VACATE 0x302
 
 /* When userland program reads or writes these already open files it
  * actually accesses the console.
